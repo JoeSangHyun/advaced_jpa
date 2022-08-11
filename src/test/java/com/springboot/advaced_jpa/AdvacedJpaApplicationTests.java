280539249d8a72eeb951f4111ba2bfdcaa7bb642
JoeSangHyun <tiamet133@naver.com>
2022-08-11 22:13:33 +0900

    Initial commit

---
+++ src/test/java/com/springboot/advaced_jpa/AdvacedJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.advaced_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvacedJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
